c0d4322c2f69ffeca13561a0254a9fba673fac43
ZZHH7890 <767580776@qq.com>
2018-04-01 10:46:04 +0800

    web auto demo

---
+++ webtest/src/main/java/coincola/webtest/cases/NewTest.java
@@ -0,0 +1,33 @@
+package coincola.webtest.cases;
+
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+
+public class NewTest {
+	WebDriver driver;
+	String testurl = "http://www.gxgg.gov.cn/";
+
+	@Test
+	public void test() {
+		System.out.println("testing");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("start");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Administrator\\eclipse-workspace\\webtest\\driver\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get(testurl);
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.close();
+		System.out.println("over");
+	}
+
+}
